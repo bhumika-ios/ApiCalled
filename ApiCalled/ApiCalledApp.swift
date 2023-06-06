57821aa36ee008cdec8c5c6ea81d567a00d2f084
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-06 14:13:58 +0530

    Initial Commit

---
+++ ApiCalled/ApiCalledApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCalledApp.swift
+//  ApiCalled
+//
+//  Created by Bhumika Patel on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiCalledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
